ed33aa37f8da0c0252db931c175f8a9b17d87f57
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-12-26 12:00:44 +0100

    cli: add env parser

--- mlonmcu/cli/env.py
+++ mlonmcu/cli/env.py
@@ -3,5 +3,9 @@
 def get_parser(subparsers):
     """"Define and return a subparser for the env subcommand."""
     parser = subparsers.add_parser('env', description='Manage ML on MCU environments.')
+    parser.set_defaults(func=handle)
     parser.add_argument('-c', '--count')
     return parser
+
+def handle(args):
+    pass

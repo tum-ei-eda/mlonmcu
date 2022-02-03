877df02001f53b7f979c4c6255c25650a50cc8a8
Philipp v. K <philipp.van-kempen@tum.de>
2022-02-03 10:03:51 +0100

    Support optimized kernel directories and libs in TFLM framework

--- mlonmcu/flow/tflite/framework.py
+++ mlonmcu/flow/tflite/framework.py
@@ -1,5 +1,7 @@
 """Definitions for TFLiteFramework."""
 
+from pathlib import Path
+
 from mlonmcu.flow.framework import Framework
 from mlonmcu.flow.tflite import TFLiteBackend
 
@@ -8,15 +10,45 @@ class TFLiteFramework(Framework):
     """TFLite Framework specialization."""
 
     name = "tflite"
+
+    FEATURES = ["muriscvnn"]
+
+    DEFAULTS = {
+        "optimized_kernel": None,
+        "optimized_kernel_inc_dir": None,
+        "optimized_kernel_lib": None,
+    }
+
+    REQUIRED = ["tf.src_dir"]
+
     backends = TFLiteBackend.registry
 
     def __init__(self, features=None, config=None):
         super().__init__(features=features, config=config)
 
+    @property
+    def tf_src(self):
+        return Path(self.config["tf.src_dir"])
+
+    @property
+    def optimized_kernel(self):
+        return self.config["optimized_kernel"]
+
+    @property
+    def optimized_kernel_lib(self):
+        return self.config["optimized_kernel_lib"]
+
+    @property
+    def optimized_kernel_inc_dir(self):
+        return self.config["optimized_kernel_inc_dir"]
+
     def get_cmake_args(self):
         args = super().get_cmake_args()
-        if "tf.src_dir" in self.config:
-            tfSrc = self.config["tf.src_dir"]
-        else:
-            raise RuntimeError("Can not resolve tf.src_dir")
-        return args + ["-DTF_SRC=" + str(tfSrc)]  # TODO: change
+        args.append(f"-DTF_SRC={self.tf_src}")
+        if self.optimized_kernel:
+            args.append(f"-DTFLM_OPTIMIZED_KERNEL={self.optimized_kernel}")
+        if self.optimized_kernel_inc_dir:
+            args.append(f"-DTFLM_OPTIMIZED_KERNEL_INCLUDE_DIR={self.optimized_kernel_inc_dir}")
+        if self.optimized_kernel_lib:
+            args.append(f"-DTFLM_OPTIMIZED_KERNEL_LIB={self.optimized_kernel_lib}")
+        return args

d536067a3ac448593ce9aac37eb4472772fbc547
Philipp v. K <phvankempen@gmail.com>
2022-03-11 21:54:36 +0100

    lint setup.py

--- setup.py
+++ setup.py
@@ -28,52 +28,52 @@ def resource_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    print("paths", paths)
+            paths.append(os.path.join("..", path, filename))
     return paths
 
-with open('README.md') as readme_file:
+
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="TUM Department of Electrical and Computer Engineering - Chair of Electronic Design Automation",
-    author_email='philipp.van-kempen@tum.de',
-    python_requires='>=3.7',
+    author_email="philipp.van-kempen@tum.de",
+    python_requires=">=3.7",
     classifiers=[  # TODO
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="This project contains research code related to the deployment of inference or learning applications on tiny micro-controllers.",
     entry_points={
-        'console_scripts': [
-            'mlonmcu=mlonmcu.cli.main:main',
+        "console_scripts": [
+            "mlonmcu=mlonmcu.cli.main:main",
         ],
     },
     install_requires=requirements,
     license="Apache License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='mlonmcu',
-    name='mlonmcu',
-    packages=find_packages(include=['mlonmcu', 'mlonmcu.*']),
+    keywords="mlonmcu",
+    name="mlonmcu",
+    packages=find_packages(include=["mlonmcu", "mlonmcu.*"]),
     package_data={"mlonmcu": resource_files("resources")},
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/tum-ei-eda/mlonmcu',
-    version='0.1.1',
+    url="https://github.com/tum-ei-eda/mlonmcu",
+    version="0.1.1",
     zip_safe=False,
 )

6f39a31cf17acad6292fc216a77859dbd63e4875
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-05-09 14:37:17 +0200

    Add new platform: tvm

--- mlonmcu/platform/_platform.py
+++ mlonmcu/platform/_platform.py
@@ -19,8 +19,10 @@
 from .platform import register_platform
 from .mlif import MlifPlatform
 from .espidf import EspIdfPlatform
+from .tvm import TvmPlatform
 
 # from .arduino import ArduinoPlatform
 
 register_platform("mlif", MlifPlatform)
 register_platform("espidf", EspIdfPlatform)
+register_platform("tvm", TvmPlatform)

432d444790964f2fb847794300f525f2905dd6f7
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-07-14 15:55:26 +0200

    Disable omit_defaults for reports...

--- mlonmcu/session/run.py
+++ mlonmcu/session/run.py
@@ -806,7 +806,8 @@ class Run:
         pre["Num"] = self.num
         post = {}
         post["Features"] = self.get_all_feature_names()
-        post["Config"] = self.get_all_configs(omit_paths=True, omit_defaults=True, omit_globals=True)
+        # post["Config"] = self.get_all_configs(omit_paths=True, omit_defaults=True, omit_globals=True)
+        post["Config"] = self.get_all_configs(omit_paths=True, omit_defaults=False, omit_globals=True)
         post["Postprocesses"] = self.get_all_postprocess_names()
         post["Comment"] = self.comment if len(self.comment) > 0 else "-"
         if self.failing:

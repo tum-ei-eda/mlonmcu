5f81cc69ba2c91d3b44668a0aba612162160bd0c
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-07-21 15:28:20 +0200

    update bench.py script

--- scripts/bench.py
+++ scripts/bench.py
@@ -12,9 +12,10 @@ config.read(ini_path)
 
 benchmarks = config.sections()
 
-assert len(sys.argv) == 3
+assert len(sys.argv) >= 3
 name = sys.argv[1]
 out_dir = sys.argv[2]
+extra_args = sys.argv[3:]
 
 assert name in benchmarks
 data = config[name]
@@ -22,6 +23,8 @@ data = config[name]
 script = data["script"]
 args = data["args"].split(" ")
 
-Path(out_dir).mkdir(exist_ok=True)
+# Path(out_dir).mkdir(exist_ok=True)
 
-subprocess.run(["python", script, *args, "--out", out_dir])
+args = ["python", script, *args, "--out", out_dir, *extra_args]
+print("Executing:", " ".join(args))
+subprocess.run(args)

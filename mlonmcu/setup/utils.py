9a2d0ff99b26792de4902276b165c727531ccb67
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-11-14 16:21:03 +0100

    lint pulp code

--- mlonmcu/setup/utils.py
+++ mlonmcu/setup/utils.py
@@ -87,6 +87,7 @@ def makeDirName(base: str, *args, flags: list = None) -> str:
         names = names + flags
     return "_".join(names)
 
+
 def exec(*args, **kwargs):
     """Execute a process with the given args and using the given kwards as Popen arguments.
 

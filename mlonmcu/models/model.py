5d021b44a6b3bf375639bcdb9e7fbb5342a1228d
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-11-29 15:48:52 +0100

    [WIP] models: allow user-provided input shapes

--- mlonmcu/models/model.py
+++ mlonmcu/models/model.py
@@ -60,9 +60,54 @@ def parse_metadata_from_path(path):
     return None
 
 
+def parse_shape_string(inputs_string):
+    """Parse an input shape dictionary string to a usable dictionary.
+
+    Taken from: https://github.com/apache/tvm/blob/main/python/tvm/driver/tvmc/shape_parser.py
+
+    Parameters
+    ----------
+    inputs_string: str
+        A string of the form "input_name:[dim1,dim2,...,dimn] input_name2:[dim1,dim2]" that
+        indicates the desired shape for specific model inputs. Colons, forward slashes and dots
+        within input_names are supported. Spaces are supported inside of dimension arrays.
+    Returns
+    -------
+    shape_dict: dict
+        A dictionary mapping input names to their shape for use in relay frontend converters.
+    """
+
+    # Create a regex pattern that extracts each separate input mapping.
+    # We want to be able to handle:
+    # * Spaces inside arrays
+    # * forward slashes inside names (but not at the beginning or end)
+    # * colons inside names (but not at the beginning or end)
+    # * dots inside names
+    pattern = r"(?:\w+\/)?[:\w.]+\:\s*\[\-?\d+(?:\,\s*\-?\d+)*\]"
+    input_mappings = re.findall(pattern, inputs_string)
+    if not input_mappings:
+        raise argparse.ArgumentTypeError(
+            "--input-shapes argument must be of the form "
+            '"input_name:[dim1,dim2,...,dimn] input_name2:[dim1,dim2]"'
+        )
+    shape_dict = {}
+    for mapping in input_mappings:
+        # Remove whitespace.
+        mapping = mapping.replace(" ", "")
+        # Split mapping into name and shape.
+        name, shape_string = mapping.rsplit(":", 1)
+        # Convert shape string into a list of integers or Anys if negative.
+        shape = [int(x) if int(x) > 0 else relay.Any() for x in shape_string.strip("][").split(",")]
+        # Add parsed mapping to shape dictionary.
+        shape_dict[name] = shape
+
+    return shape_dict
+
+
 class Model:
     DEFAULTS = {
         "metadata_path": "definition.yml",
+        "input_shapes": None,
         "support_path": "support",
         "inputs_path": "input",
         "outputs_path": "output",
@@ -83,6 +128,16 @@ class Model:
     @property
     def metadata_path(self):
         return self.config["metadata_path"]
+
+    @property
+    def input_shapes(self):
+        temp = self.config["input_shapes"]
+        if temp:
+            if isinstance(temp, str):
+                temp = parse_shape_string(temp)
+            else:
+                assert isinstance(temp, dict)
+        return temp
 
     @property
     def support_path(self):

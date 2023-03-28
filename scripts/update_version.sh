7c4bd50f8f3b79053b211e9f9731dfa329fdbd7a
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-03-28 14:35:41 +0200

    improve update_version.sh script

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DEV_VERSION=$(git describe --tags --match "v[0-9]*.[0-9]*.[0-9]*" --match "v[0-9]*.[0-9]*.dev[0-9]*" | cut -d'-' -f1-2 | sed 's/0\-//')
+DEV_VERSION=$(git describe --tags --match "v[0-9]*.[0-9]*.[0-9]*" --match "v[0-9]*.[0-9]*.dev[0-9]*" | cut -d'-' -f1-2 | sed 's/0\-//' | sed 's/dev\0$/0/')
 
 head -n -1 mlonmcu/version.py > mlonmcu/version.py.new
 echo "__version__ = \"${DEV_VERSION:1}\"" >> mlonmcu/version.py.new

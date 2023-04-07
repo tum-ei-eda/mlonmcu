4590b7f9af1f5b69ffcbafba5b0ac644d469ec72
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-04-07 16:17:16 +0200

    add missing license headers

--- mlonmcu/platform/microtvm/microtvm_tune_platform.py
+++ mlonmcu/platform/microtvm/microtvm_tune_platform.py
@@ -1,11 +1,25 @@
+#
+# Copyright (c) 2022 TUM Department of Electrical and Computer Engineering.
+#
+# This file is part of MLonMCU.
+# See https://github.com/tum-ei-eda/mlonmcu.git for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""MicroTVM Tune Platform"""
 from mlonmcu.config import str2bool
 from .microtvm_target_platform import MicroTvmTargetPlatform
 from ..tvm.tvm_tune_platform import TvmTunePlatform
-
-from mlonmcu.flow.tvm.backend.tvmc_utils import (
-    get_rpc_tvmc_args,
-    get_target_tvmc_args,
-)
 
 
 class MicroTvmTunePlatform(TvmTunePlatform, MicroTvmTargetPlatform):

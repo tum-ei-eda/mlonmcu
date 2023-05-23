6b701ee24a91f030d95e7ec176b2866a4f27010c
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-05-23 11:00:43 +0200

    mlonmcu export: copy report.csv as well

--- mlonmcu/context/context.py
+++ mlonmcu/context/context.py
@@ -504,9 +504,13 @@ class MlonMcuContext:
                     base = tmpdir / str(sid)
                 if run_ids is None:
                     src = session.dir / "runs"
+                    report_path = session.dir / "report.csv"  # TODO: only works for csv
+                    # TODO: use artifacts from restored session instead
                     shutil.copytree(
                         src, base, dirs_exist_ok=True, symlinks=True
                     )  # Warning: dirs_exist_ok=True requires python 3.8+
+                    if report_path.is_file():
+                        shutil.copyfile(report_path, base)
                 else:
                     base = base / "runs"
                     for rid in run_ids:

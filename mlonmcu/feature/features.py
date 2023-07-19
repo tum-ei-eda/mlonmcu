fb4970228127686fa043c4a228276d3fd54083e9
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-07-19 22:18:23 +0200

    support gdbserver feature for corev_ovpsim

--- mlonmcu/feature/features.py
+++ mlonmcu/feature/features.py
@@ -571,7 +571,7 @@ class GdbServer(TargetFeature):
         return int(self.config["port"]) if self.config["port"] is not None else None
 
     def get_target_config(self, target):
-        assert target in ["host_x86", "etiss_pulpino", "etiss", "ovpsim"]
+        assert target in ["host_x86", "etiss_pulpino", "etiss", "ovpsim", "corev_ovpsim"]
         return filter_none(
             {
                 f"{target}.gdbserver_enable": self.enabled,

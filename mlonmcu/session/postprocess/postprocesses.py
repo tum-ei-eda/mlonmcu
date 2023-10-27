d70e2b594271a8dd2fe98554d62f876498daef56
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-10-27 14:38:26 +0200

    postprocesses.py: remove duplicate property

--- mlonmcu/session/postprocess/postprocesses.py
+++ mlonmcu/session/postprocess/postprocesses.py
@@ -629,12 +629,6 @@ class CompareRowsPostprocess(SessionPostprocess):
         return int(value)
 
     @property
-    def baseline(self):
-        """Get baseline property."""
-        value = self.config["baseline"]
-        return int(value)
-
-    @property
     def percent(self):
         """Get percent property."""
         value = self.config["percent"]

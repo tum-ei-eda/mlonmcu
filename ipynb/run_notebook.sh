9fb3063382c52d2e12d1948c6ee7248c8fa51c21
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-12-29 23:50:48 +0100

    ipynb: updates

--- ipynb/run_notebook.sh
+++ ipynb/run_notebook.sh
@@ -15,6 +15,8 @@ SKIP=0
 CLEANUP=0
 CLEAR=0
 NOOP=0
+HTML=1
+PDF=1
 
 NOTEBOOK=$(readlink -f $1)
 DIR=$(readlink -f $(dirname $0))
@@ -35,18 +37,18 @@ then
 else
     ENVIRONMENT=$TEMPDIR/venv
 fi
-env
 cd $TEMPDIR
 
 if [[ "$ENVIRONMENT" != "custom" ]]
 then
     echo "Configuring python..."
-    export PYTHONPATH=$DIR/.. # TODO
+    # export PYTHONPATH=$DIR/.. # TODO
     if [[ $SKIP -eq 0 ]]
     then
         echo "Creating virtualenv..."
         python3 -m venv $ENVIRONMENT
         source $ENVIRONMENT/bin/activate
+        python3 -m pip install -e $DIR/..
         if [[ -f $REQUIREMENTS ]]
         then
             echo "(Using provided requirements)"
@@ -70,10 +72,10 @@ then
         if [[ -f $YAML ]]
         then
             echo "(Using provided template)"
-            python3 -m mlonmcu.cli.main init -t $YAML $WORKSPACE --non-interactive --clone-models
+            python3 -m mlonmcu.cli.main init -t $YAML $WORKSPACE --non-interactive --clone-models --allow-exists
         else
             echo "(No template found. Falling back to default...)"
-            python3 -m mlonmcu.cli.main init -t default $WORKSPACE --non-interactive --clone-models
+            python3 -m mlonmcu.cli.main init -t default $WORKSPACE --non-interactive --clone-models --allow-exists
         fi
         echo "Setting up MLonMCU environment..."
         python3 -m mlonmcu.cli.main setup -v
@@ -87,6 +89,19 @@ then
 else
     echo "Skipping execution of notebook..."
 fi
+
+if [[ $HTML -eq 1 ]]
+then
+    echo "Converting notebook to HTML..."
+    python3 -m jupyter nbconvert --to html $NOTEBOOK
+fi
+
+if [[ $PDF -eq 1 ]]
+then
+    echo "Converting notebook to PDF..."
+    python3 -m jupyter nbconvert --to pdf $NOTEBOOK
+fi
+
 
 if [[ $CLEAR -eq 1 ]]
 then

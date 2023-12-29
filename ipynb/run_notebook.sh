e76c40a194161eaeef1995f40b84ae7a4d51452d
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-12-29 15:28:39 +0100

    ipynb: add run_notebook.sh script

--- ipynb/run_notebook.sh
+++ ipynb/run_notebook.sh
@@ -1,12 +1,105 @@
 #!/bin/bash
 
-NOTEBOOK=$1
+set -e
+
+if [[ $# -lt 1 ]]
+then
+    echo "Invalid number or arguments!"
+    echo "Usage: $0 path/to/notebook.ipynb [-h MLONMCU_HOME] [-e VENV] [--skip] [--cleanup] [--clear] [--noop]"
+    exit 1
+fi
+
+HOME=
+VENV=
+SKIP=0
+CLEANUP=0
+CLEAR=0
+NOOP=0
+
+NOTEBOOK=$(readlink -f $1)
+DIR=$(readlink -f $(dirname $0))
+NAME=$(basename $NOTEBOOK | cut -d. -f1)
 DIRECTORY=$(dirname $NOTEBOOK)
 YAML=$DIRECTORY/environment.yml.j2
-REQUIREMTS=
+REQUIREMENTS=$DIRECTORY/requirements.txt
+TEMPDIR=$(mktemp -d -t $NAME-XXXX)
+if [[ "$HOME" != "" ]]
+then
+    WORKSPACE=$HOME
+else
+    WORKSPACE=$TEMPDIR/workspace
+fi
+if [[ "$VENV" != "" ]]
+then
+    ENVIRONMENT=$VENV
+else
+    ENVIRONMENT=$TEMPDIR/venv
+fi
+env
+cd $TEMPDIR
 
-mlonmcu init -t
+if [[ "$ENVIRONMENT" != "custom" ]]
+then
+    echo "Configuring python..."
+    export PYTHONPATH=$DIR/.. # TODO
+    if [[ $SKIP -eq 0 ]]
+    then
+        echo "Creating virtualenv..."
+        python3 -m venv $ENVIRONMENT
+        source $ENVIRONMENT/bin/activate
+        if [[ -f $REQUIREMENTS ]]
+        then
+            echo "(Using provided requirements)"
+            python3 -m pip install -r $REQUIREMENTS > /dev/null
+        fi
+        elif [[ -f $DIR/requirements.txt ]]
+        then
+            echo "(No requirements found. Falling back to default requirements.txt)"
+            python3 -m pip install -r $DIR/requirements.txt > /dev/null
+    else
+        echo "Skipping creation of virtualenv..."
+    fi
+fi
+echo "PYTHONPATH=$PYTHONPATH"
+if [[ "$WORKSPACE" != "custom" ]]
+then
+    export MLONMCU_HOME=$WORKSPACE
+    if [[ $SKIP -eq 0 ]]
+    then
+        echo "Initializing MLonMCU environment..."
+        if [[ -f $YAML ]]
+        then
+            echo "(Using provided template)"
+            python3 -m mlonmcu.cli.main init -t $YAML $WORKSPACE --non-interactive --clone-models
+        else
+            echo "(No template found. Falling back to default...)"
+            python3 -m mlonmcu.cli.main init -t default $WORKSPACE --non-interactive --clone-models
+        fi
+        echo "Setting up MLonMCU environment..."
+        python3 -m mlonmcu.cli.main setup -v
+    fi
+fi
 
+if [[ $NOOP -eq 0 ]]
+then
+    echo "Executing notebook..."
+    python3 -m jupyter nbconvert --to notebook --execute $NOTEBOOK
+else
+    echo "Skipping execution of notebook..."
+fi
 
+if [[ $CLEAR -eq 1 ]]
+then
+    echo "Clearing output cells..."
+    python3 -m jupyter nbconvert --clear-output --inplace $NOTEBOOK
+fi
 
-python -m jupyter nbconvert --to notebook --execute AnalyseInstructionsFeature.ipynb
+cd -
+
+if [[ $CLEANUP -eq 1 ]]
+then
+    echo "Cleaning up..."
+    rm -r $TEMPDIR
+fi
+
+echo "Done."

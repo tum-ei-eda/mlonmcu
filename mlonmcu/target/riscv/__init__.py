c461e2b5128ede009c897dfea6d7dd782926c365
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-01-08 09:29:13 +0100

    Add experimental ara_rtl target (questasim) 2

--- mlonmcu/target/riscv/__init__.py
+++ mlonmcu/target/riscv/__init__.py
@@ -6,6 +6,7 @@ from .corev_ovpsim import COREVOVPSimTarget
 from .riscv_qemu import RiscvQemuTarget
 from .gvsoc_pulp import GvsocPulpTarget
 from .ara import AraTarget
+from .ara_rtl import AraRtlTarget
 from .cv32e40p import CV32E40PTarget
 from .vicuna import VicunaTarget
 

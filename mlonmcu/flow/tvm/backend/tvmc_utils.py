a87b42a63788f912b28a8bdc6a1d626c7a106608
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-01-15 09:46:07 +0100

    fix print_top dtype

--- mlonmcu/flow/tvm/backend/tvmc_utils.py
+++ mlonmcu/flow/tvm/backend/tvmc_utils.py
@@ -176,7 +176,7 @@ def get_data_tvmc_args(mode=None, ins_file=None, outs_file=None, print_top=10):
         ret.extend(["--outputs", outs_file])
 
     if print_top is not None and print_top > 0:
-        ret.extend(["--print-top", print_top])
+        ret.extend(["--print-top", str(print_top)])
 
     return ret
 

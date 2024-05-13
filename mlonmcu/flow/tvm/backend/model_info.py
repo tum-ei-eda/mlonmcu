523f92e56dd208692fac10b86a2a8812d3f23941
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-05-13 15:04:49 +0200

    model_info: fix generated onnx output tensor names

--- mlonmcu/flow/tvm/backend/model_info.py
+++ mlonmcu/flow/tvm/backend/model_info.py
@@ -247,8 +247,11 @@ class ONNXModelInfo(ModelInfo):
         out_tensors = _helper(model.graph.output)
 
         # TVM seems to ignore the original output names for ONNX models
-        for i, t in enumerate(out_tensors):
-            t.name = f"output{i}"
+        if len(out_tensors) == 1:
+            out_tensors[0].name = "output"
+        else:
+            for i, t in enumerate(out_tensors):
+                t.name = f"output{i}"
         super().__init__(in_tensors, out_tensors)
 
 

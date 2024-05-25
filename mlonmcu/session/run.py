e284efe07fc712f86f389122521c602e35242f69
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-05-25 08:18:16 +0200

    run: fix handling of missing sub artifacts

--- mlonmcu/session/run.py
+++ mlonmcu/session/run.py
@@ -828,7 +828,7 @@ class Run:
     @property
     def artifacts(self):
         sub = "default"
-        ret = sum(list(itertools.chain([subs[sub] for stage, subs in self.artifacts_per_stage.items()])), [])
+        ret = sum(list(itertools.chain([subs[sub] for stage, subs in self.artifacts_per_stage.items() if sub in subs])), [])
         return ret
 
     def get_all_sub_artifacts(self, sub, stage=None):

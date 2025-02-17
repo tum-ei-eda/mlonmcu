b4d9b65977a62cdd7b1e3a1ff3a0779f219c293a
Jan-Eric Schäfrich <j.schaefrich@tum.de>
2025-02-17 11:23:22 +0100

    add muriscvnn.use_corev property

--- mlonmcu/feature/features.py
+++ mlonmcu/feature/features.py
@@ -156,6 +156,7 @@ class Muriscvnn(SetupFeature, FrameworkFeature, PlatformFeature):
         **FeatureBase.DEFAULTS,
         "use_vext": "AUTO",
         "use_pext": "AUTO",
+        "use_corev": False,
     }
 
     REQUIRED = {"muriscvnn.src_dir"}
@@ -182,6 +183,12 @@ class Muriscvnn(SetupFeature, FrameworkFeature, PlatformFeature):
             return value
         value = str2bool(value)
         return "ON" if value else "OFF"
+    
+    @property
+    def use_corev(self):
+        value = self.config["use_corev"]
+        value = str2bool(value)
+        return "ON" if value else "OFF"
 
     def add_framework_config(self, framework, config):
         assert framework == "tflm", f"Unsupported feature '{self.name}' for framework '{framework}'"
@@ -200,6 +207,7 @@ class Muriscvnn(SetupFeature, FrameworkFeature, PlatformFeature):
             "MURISCVNN_DIR": self.muriscvnn_dir,
             "MURISCVNN_VEXT": self.use_vext,
             "MURISCVNN_PEXT": self.use_pext,
+            "MURISCVNN_COREV": self.use_corev,
         }
 
     def get_required_cache_flags(self):

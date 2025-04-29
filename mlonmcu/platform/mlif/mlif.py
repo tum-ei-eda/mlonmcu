36e46e2cecf3a0f75101c0bdf8b00787cd51ede8
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-04-29 08:45:05 +0200

    mlonmcu/platform/mlif/mlif.py: support iree framework

--- mlonmcu/platform/mlif/mlif.py
+++ mlonmcu/platform/mlif/mlif.py
@@ -105,7 +105,7 @@ class MlifPlatform(CompilePlatform, TargetPlatform):
     }
 
     REQUIRED = {"mlif.src_dir"}
-    OPTIONAL = {"llvm.install_dir", "srecord.install_dir"}
+    OPTIONAL = {"llvm.install_dir", "srecord.install_dir", "iree.install_dir"}
 
     def __init__(self, features=None, config=None):
         super().__init__(
@@ -265,6 +265,10 @@ class MlifPlatform(CompilePlatform, TargetPlatform):
     @property
     def srecord_dir(self):
         return self.config["srecord.install_dir"]
+
+    @property
+    def iree_install_dir(self):
+        return self.config["iree.install_dir"]
 
     @property
     def template(self):
@@ -460,6 +464,11 @@ class MlifPlatform(CompilePlatform, TargetPlatform):
         if self.srecord_dir:
             path_old = env["PATH"]
             path_new = f"{self.srecord_dir}:{path_old}"
+            env["PATH"] = path_new
+        # TODO: refactor
+        if self.iree_install_dir:
+            path_old = env["PATH"]
+            path_new = f"{self.iree_install_dir}/bin:{path_old}"
             env["PATH"] = path_new
         return env
 

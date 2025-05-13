df1b0831e7c0d18e8d8b11185f7dfff27d7f9bf8
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-05-13 07:33:24 +0200

    add {model_name}.params_path to overwrite relay params (needs patched TVMC)

--- mlonmcu/session/run.py
+++ mlonmcu/session/run.py
@@ -907,12 +907,14 @@ class Run:
                     output_shapes = self.model.output_shapes
                     input_types = self.model.input_types
                     output_types = self.model.output_types
+                    params_path = self.model.params_path
                 self.backend.load_model(
                     model=model_artifact.path,
                     input_shapes=input_shapes,
                     output_shapes=output_shapes,
                     input_types=input_types,
                     output_types=output_types,
+                    params_path=params_path,
                 )
                 _build()
 
@@ -932,17 +934,22 @@ class Run:
                 output_shapes = None
                 input_types = None
                 output_types = None
+                print("self.model", self.model)
+                print("model_artifact.name", model_artifact.name)
                 if model_artifact.name.split(".", 1)[0] == self.model.name:
                     input_shapes = self.model.input_shapes
                     output_shapes = self.model.output_shapes
                     input_types = self.model.input_types
                     output_types = self.model.output_types
+                    params_path = self.model.params_path
+                print("params_path", params_path)
                 self.backend.load_model(
                     model=model_artifact.path,
                     input_shapes=input_shapes,
                     output_shapes=output_shapes,
                     input_types=input_types,
                     output_types=output_types,
+                    params_path=params_path,
                 )
                 _build()
 

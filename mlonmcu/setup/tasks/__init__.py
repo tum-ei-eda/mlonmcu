c490f562805c8dd041aeaca8ef9e2ba162b7c30d
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-05-29 21:14:03 +0200

    fixes

--- mlonmcu/setup/tasks/__init__.py
+++ mlonmcu/setup/tasks/__init__.py
@@ -45,6 +45,7 @@ from .ovpsim import *  # noqa: F401, F403
 from .vicuna import *  # noqa: F401, F403
 from .benchmarks import *  # noqa: F401, F403
 from .srecord import *  # noqa: F401, F403
+
 # from .layergen import *  # noqa: F401, F403
 from .dtc import *  # noqa: F401, F403
 from .corev import *  # noqa: F401, F403

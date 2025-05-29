ecde330311b4c4cc75fdd2c55442120b58c4ca89
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-05-29 14:15:46 +0200

    fix espidf python setup

--- mlonmcu/setup/tasks/espidf.py
+++ mlonmcu/setup/tasks/espidf.py
@@ -26,6 +26,7 @@ from mlonmcu.setup.task import TaskType
 from mlonmcu.context.context import MlonMcuContext
 from mlonmcu.setup import utils
 from mlonmcu.logging import get_logger
+from mlonmcu.utils import get_base_prefix_compat
 
 from .common import get_task_factory
 
@@ -57,7 +58,7 @@ def clone_espidf(
 
 
 @Tasks.needs(["espidf.src_dir"])
-@Tasks.provides(["espidf.install_dir"])
+@Tasks.provides(["espidf.install_dir", "espidf.python_dir"])
 @Tasks.validate(_validate_espidf)
 @Tasks.register(category=TaskType.PLATFORM)
 def install_espidf(
@@ -66,8 +67,10 @@ def install_espidf(
     """Download and install target support for ESP-IDF toolchain."""
     espidfName = utils.makeDirName("espidf")
     espidfInstallDir = context.environment.paths["deps"].path / "install" / espidfName
+    espidfPythonDir = espidfInstallDir / "python_env"
     espidfSrcDir = context.cache["espidf.src_dir"]  # TODO: This will fail if the espidf.src_dir is user-supplied
     user_vars = context.environment.vars
+    # TODO: python version in bash needs to match the one of the current venv!
     if "espidf.install_dir" in user_vars:  # TODO: also check command line flags?
         return False
     boards = ["all"]
@@ -76,13 +79,27 @@ def install_espidf(
     if not isinstance(boards, str):
         assert isinstance(boards, list)
         boards = ",".join(boards)
-    if not utils.is_populated(espidfInstallDir) or rebuild:
+    if not utils.is_populated(espidfInstallDir) or not utils.is_populated(espidfPythonDir) or rebuild:
         # Using idf_tools.py directory instead of ./install.sh because we
         # don't want to use espe-idfs python environment
         espidfInstallScript = Path(espidfSrcDir) / "tools" / "idf_tools.py"
         espidfInstallArgs = ["install", f"--targets={boards}"]
+        espidfPythonArgs = ["install-python-env"]
         env = os.environ.copy()
         env["IDF_TOOLS_PATH"] = str(espidfInstallDir)
+        path_orig = env["PATH"]
+
+        # workaround to use system python (matching venv version)
+        import sys
+
+        PYTHON_VER_MAJOR_MINOR = f"{sys.version_info.major}.{sys.version_info.minor}"
+        python_exe = f"python{PYTHON_VER_MAJOR_MINOR}"
+        python_dir = get_base_prefix_compat() + "/bin"
+        python_exe_full = f"{python_dir}/{python_exe}"
+        path_new = f"{python_dir}:{path_orig}"
+        env["PATH"] = path_new
         utils.python(espidfInstallScript, *espidfInstallArgs, live=verbose, env=env)
+        utils.execute(python_exe_full, espidfInstallScript, *espidfPythonArgs, live=verbose, env=env)
     context.cache["espidf.install_dir"] = espidfInstallDir
+    context.cache["espidf.python_dir"] = espidfPythonDir
     context.export_paths.add(espidfInstallDir / "bin")

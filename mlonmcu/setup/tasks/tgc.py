f81d93a643f3c7eb314e97051223124e250fc7b8
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-06-03 07:58:26 +0200

    tgc setup fixes

--- mlonmcu/setup/tasks/tgc.py
+++ mlonmcu/setup/tasks/tgc.py
@@ -38,7 +38,7 @@ def _validate_tgc(context: MlonMcuContext, params=None):
         return False
     user_vars = context.environment.vars
     if "tgc.exe" not in user_vars:  # TODO: also check command line flags?
-        assert "spike" in context.environment.repos, "Undefined repository: 'spike'"
+        assert "tgc" in context.environment.repos, "Undefined repository: 'tgc'"
     return True
 
 
@@ -132,7 +132,7 @@ def build_tgc(context: MlonMcuContext, params=None, rebuild=False, verbose=False
         # No need to build a vext and non-vext variant?
         utils.mkdirs(tgcBuildDir)
         if "tgc_gen" in context.environment.repos:
-            utils.exec_getout(
+            utils.execute(
                 "direnv allow",
                 cwd=tgcSrcDir,
                 shell=True,
@@ -146,13 +146,13 @@ def build_tgc(context: MlonMcuContext, params=None, rebuild=False, verbose=False
                 #         cmd = f'/bin/bash -c "source ~/.sdkman/bin/sdkman-init.sh && sdk use java 11.0.21-tem && java
                 # --version && {tgcSrcDir}/../tgc_gen/scripts/generate_iss.sh -c {version} -o dbt-rise-tgc/ -b {backend}
                 # {tgcSrcDir}/../tgc_gen/CoreDSL/{version}.core_desc"'
-                #         utils.exec_getout(
+                #         utils.execute(
                 #             cmd,
                 #             cwd=tgcSrcDir,
                 #             print_output=True,
                 #             shell=True,
                 #         )
-        utils.exec_getout(
+        utils.execute(
             "cmake",
             "-S",
             tgcSrcDir,

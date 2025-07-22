8e7b31ecf6a61440e95d276c58abdcfeed50d344
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-07-22 09:55:56 +0200

    lint

--- mlonmcu/session/postprocess/dwarf.py
+++ mlonmcu/session/postprocess/dwarf.py
@@ -16,11 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import sys
-import logging
-import argparse
 import posixpath
-from pathlib import Path
 from collections import defaultdict
 
 import pandas as pd

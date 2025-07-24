86e194b3c3a3148c85ae9a5cf73c8296b9fb4368
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-07-24 20:10:03 +0200

    mlonmcu/flow/tvm/backend/tvmc_utils.py: drop target-c-constants-byte-alignment

--- mlonmcu/flow/tvm/backend/tvmc_utils.py
+++ mlonmcu/flow/tvm/backend/tvmc_utils.py
@@ -159,8 +159,8 @@ def get_tvmaot_tvmc_args(alignment_bytes, unpacked_api, runtime="crt", target="c
     ]
     if target == "c":
         ret += [
-            *["--target-c-constants-byte-alignment", str(alignment_bytes)],
-            *["--target-c-workspace-byte-alignment", str(alignment_bytes)],
+            # *["--target-c-constants-byte-alignment", str(alignment_bytes)],
+            # *["--target-c-workspace-byte-alignment", str(alignment_bytes)],
             *["--executor-aot-constant-byte-alignment", str(alignment_bytes)],
             *["--executor-aot-workspace-byte-alignment", str(alignment_bytes)],
         ]

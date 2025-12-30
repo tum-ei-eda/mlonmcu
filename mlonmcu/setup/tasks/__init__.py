8b98ed3ffc81fb6a6350702aae9feb635506f1da
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-12-30 21:54:46 +0100

    add mlonmcu/setup/tasks/yosys.py

--- mlonmcu/setup/tasks/__init__.py
+++ mlonmcu/setup/tasks/__init__.py
@@ -53,3 +53,4 @@ from .cv32e40p import *  # noqa: F401, F403
 from .boost import *  # noqa: F401, F403
 from .cmake import *  # noqa: F401, F403
 from .tgc import *  # noqa: F401, F403
+from .yosys import *  # noqa: F401, F403

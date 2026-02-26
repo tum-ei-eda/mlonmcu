ae1688b70af4eac28ab058c96339e3909e4dbb07
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-26 10:38:13 +0100

    fix merge conflict

--- mlonmcu/config.py
+++ mlonmcu/config.py
@@ -64,15 +64,7 @@ def remove_config_prefix(config, prefix, skip=None):
         return key.split(".", 1)[1]
 
     # return {helper(key): value for key, value in config.items() if f"{prefix}." in key and key not in skip}
-<<<<<<< HEAD
-    return {
-        helper(key): value
-        for key, value in config.items()
-        if matches(key)
-    }
-=======
     return {helper(key): value for key, value in config.items() if matches(key)}
->>>>>>> develop
 
 
 def filter_config(config, prefix, defaults, optionals, required_keys):

3a7259262779270de3e9f0c162be9239e5228516
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-03-03 09:50:17 +0100

    lint

--- mlonmcu/flow/__init__.py
+++ mlonmcu/flow/__init__.py
@@ -18,34 +18,6 @@
 #
 """Flow module for frameworks and backend."""
 
-### from mlonmcu.flow.tflm.framework import TFLMFramework
-### from mlonmcu.flow.tvm.framework import TVMFramework
-### from mlonmcu.flow.iree.framework import IREEFramework
-### from mlonmcu.flow.emx.framework import EMXFramework
-### from mlonmcu.flow.executorch.framework import ExecutorchFramework
-###
-### # from mlonmcu.flow.none.framework import NoneFramework
-###
-### from mlonmcu.flow.tflm.backend.tflmc import TFLMCBackend
-### from mlonmcu.flow.tflm.backend.tflmi import TFLMIBackend
-###
-### from mlonmcu.flow.tvm.backend.tvmaot import TVMAOTBackend
-### from mlonmcu.flow.tvm.backend.tvmaotplus import TVMAOTPlusBackend
-### from mlonmcu.flow.tvm.backend.tvmcg import TVMCGBackend
-### from mlonmcu.flow.tvm.backend.tvmllvm import TVMLLVMBackend
-### from mlonmcu.flow.tvm.backend.tvmrt import TVMRTBackend
-###
-### from mlonmcu.flow.iree.backend.ireevmvx import IREEVMVXBackend
-### from mlonmcu.flow.iree.backend.ireevmvx_inline import IREEVMVXInlineBackend
-### from mlonmcu.flow.iree.backend.ireellvm import IREELLVMBackend
-### from mlonmcu.flow.iree.backend.ireellvm_inline import IREELLVMInlineBackend
-### from mlonmcu.flow.iree.backend.ireellvmc import IREELLVMCBackend
-### from mlonmcu.flow.iree.backend.ireellvmc_inline import IREELLVMCInlineBackend
-###
-### from mlonmcu.flow.emx.backend import EMXBackend
-### from mlonmcu.flow.executorch.backend import ExecutorchBackend
-
-# from mlonmcu.flow.none.backend.none import NoneBackend
 from .registry import SUPPORTED_TVM_BACKENDS, SUPPORTED_FRAMEWORKS, SUPPORTED_BACKENDS, get_available_backend_names
 from .framework import Framework
 from .backend import Backend

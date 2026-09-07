4cf4f08cd7aaac6409e095ff23de7b5f3830e559
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-09-07 12:02:38 +0200

    Revert "mlonmcu/flow/tvm/backend/wrapper.py: drop dummy TVMFuncRegisterGlobal for tvmaotplus backend (sw now links tvm_runtime_api.c)"
    This reverts commit 5ce0b19c547aa426b3dbf96f3813b435391ea0c8.

--- mlonmcu/flow/tvm/backend/wrapper.py
+++ mlonmcu/flow/tvm/backend/wrapper.py
@@ -538,6 +538,11 @@ void __attribute__((noreturn)) TVMPlatformAbort(tvm_crt_error_t code)
     mlonmcu_exit(1);
 }
 
+TVM_DLL int TVMFuncRegisterGlobal(const char* name, TVMFunctionHandle f, int override)
+{
+    return 0;
+}
+
 int TVMWrap_Init()
 {
 """
